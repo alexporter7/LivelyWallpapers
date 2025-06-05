05c29d92c9dc868a545bb858e2df0de56b4cf29c
Alex <alex.porter7@outlook.com>
2025-06-05 08:07:09 -0500

    Added additional config options

--- MatrixImproved/script.js
+++ MatrixImproved/script.js
@@ -1,33 +1,41 @@
-var root = {
-  wavecolor: {  
-    r: 125,
-    g: 52,
-    b: 253
+let root = {
+    waveColor: {
+        r: 125,
+        g: 52,
+        b: 253
     },
     rainbowSpeed: 0.5,
     rainbow: true,
-    matrixspeed: 50
+    matrixSpeed: 50,
+    useCustomCharacters: false,
+    customCharacterSet: "",
+    fontSize: 14
 };
 
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
+let defaultCharacterSet
+    = "゠アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレワヰヱヲンヺ・ーヽヿ0123456789";
 
-var hueFw = false;
-var hue = -0.01;
+let c = document.getElementById("c");
+let ctx = c.getContext("2d");
+
+let hueFw = false;
+let hue = -0.01;
 
 // making the canvas full screen
 c.height = window.innerHeight;
 c.width = window.innerWidth;
 
-// the characters
-var konkani  = "゠アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレワヰヱヲンヺ・ーヽヿ0123456789"
-// converting the string into an array of single characters
-var characters = konkani.split("");
-var font_size = 14;
-var columns = c.width/font_size;    // number of columns for the rain
-var gradient = ctx.createLinearGradient(0,10, 0,200);
+let konkani = (root.useCustomCharacters)
+    ? root.customCharacterSet
+    : defaultCharacterSet;
+let characters = konkani.split("");
+let font_size = root.fontSize;
+
+let columns = c.width / font_size;    // number of columns for the rain
+let gradient = ctx.createLinearGradient(0, 10, 0, 200);
+
 // an array of drops - one per column
-var drops = [];
+let drops = [];
 // x below is the x coordinate
 // 1 = y-coordinate of the drop (same for every drop initially)
 for (var x = 0; x < columns; x++)
@@ -42,65 +50,81 @@ function draw() {
     ctx.fillRect(0, 0, c.width, c.height);
 
     ctx.fillStyle = "#BBB"; // grey text
-    ctx.font = font_size + "px arial";
+    ctx.font = root.fontSize + "px arial";
+
 
     // looping over drops
-    for (var i = 0; i < drops.length; i++)
-    {
+    for (var i = 0; i < drops.length; i++) {
         // background color
         ctx.fillStyle = "rgba(10,10,10, 1)";
-        ctx.fillRect(i * font_size, drops[i] * font_size,font_size,font_size);
+        ctx.fillRect(i * root.fontSize, drops[i] * root.fontSize, root.fontSize, root.fontSize);
         // a random chinese character to print
         var text = characters[Math.floor(Math.random() * characters.length)];
         // x = i * font_size, y = value of drops[i] * font_size
 
         if (root.rainbow) {
-          hue += (hueFw) ? 0.01 : -0.01;
-          var rr = Math.floor(127 * Math.sin(root.rainbowSpeed * hue + 0) + 128);
-          var rg = Math.floor(127 * Math.sin(root.rainbowSpeed * hue + 2) + 128);
-          var rb = Math.floor(127 * Math.sin(root.rainbowSpeed * hue + 4) + 128);
-          ctx.fillStyle = 'rgba(' + rr + ',' + rg + ',' + rb + ')';
+            hue += (hueFw) ? 0.01 : -0.01;
+            var rr = Math.floor(127 * Math.sin(root.rainbowSpeed * hue + 0) + 128);
+            var rg = Math.floor(127 * Math.sin(root.rainbowSpeed * hue + 2) + 128);
+            var rb = Math.floor(127 * Math.sin(root.rainbowSpeed * hue + 4) + 128);
+            ctx.fillStyle = 'rgba(' + rr + ',' + rg + ',' + rb + ')';
         } else {
-          ctx.fillStyle = 'rgba(' + root.wavecolor.r + ',' + root.wavecolor.g + ',' + root.wavecolor.b + ')';
+            ctx.fillStyle = 'rgba(' + root.waveColor.r + ',' + root.waveColor.g + ',' + root.waveColor.b + ')';
         }
 
-        ctx.fillText(text, i * font_size, drops[i] * font_size);
+        ctx.fillText(text, i * root.fontSize, drops[i] * root.fontSize);
         // Incrementing Y coordinate
         drops[i]++;
         // sending the drop back to the top randomly after it has crossed the screen
         // adding randomness to the reset to make the drops scattered on the Y axis
-       if (drops[i] * font_size > c.height && Math.random() > 0.975)
-			      drops[i] = 0;
+        if (drops[i] * root.fontSize > c.height && Math.random() > 0.975)
+            drops[i] = 0;
     }
 }
 
-window.onresize=()=>{
+window.onresize = () => {
     location.reload();
 }
 
-setInterval(draw, root.matrixspeed);
+setInterval(draw, root.matrixSpeed);
 
-function livelyPropertyListener(name, val)
-{
-  switch(name) {
-    case "matrixColor":
-      root.wavecolor =  hexToRgb(val);
-      break;
-    case "rainBow":
-      root.rainbow = val;
-      break;   
-    case "rainbowSpeed":
-      root.rainbowSpeed = val/100;
-      break;     
-  }
+function livelyPropertyListener(name, val) {
+    switch (name) {
+        case "matrixColor":
+            root.waveColor = hexToRgb(val);
+            break;
+        case "rainBow":
+            root.rainbow = val;
+            break;
+        case "rainbowSpeed":
+            root.rainbowSpeed = val / 100;
+            break;
+        case "useCustomCharacters":
+            root.useCustomCharacters = val;
+            break;
+        case "customCharacters":
+            root.customCharacterSet = val;
+            break;
+    }
+    updatePropertyValues();
+}
+
+function updatePropertyValues() {
+    konkani = (root.useCustomCharacters)
+        ? root.customCharacterSet
+        : defaultCharacterSet;
+    characters = konkani.split("");
+    font_size = root.fontSize;
+
+    columns = c.width / font_size;    // number of columns for the rain
 }
 
 function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
 }
 
